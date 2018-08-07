44078bc39984d31e772682e16ac05d14cdeba378
mritten <mritten@gmail.com>
2018-08-06 21:38:01 -0400

    Javascript Lab 1.1

--- lab.js
+++ lab.js
@@ -3,17 +3,17 @@ const question = prompt("Would you like to play a game?");
 if (question === "yes"||"y"){
 const playerName = prompt(`What is your name?`);
 
-let playerHealth = 40;
+let playerHealth=40;
 let grant = "Grant the Mighty Chicken";
-let grantHealth = 10;
-let winCount = 0;
+let grantHealth=10;
+let winCount=0;
 
 
 while (playerHealth >=0 && winCount < 3) {
   playerHealth -= Math.floor((Math.random() * 2) + 1);
   grantHealth -= Math.floor((Math.random() * 2) + 1);
-console.log(`${playerName} has ${playerHealth} left`)
-console.log(`${grant} has ${grantHealth} left`)
+console.log(`${playerName} has ${playerHealth} life left`)
+console.log(`${grant} has ${grantHealth} life left`)
 
 if (grantHealth <= 0) {
   winCount++;
